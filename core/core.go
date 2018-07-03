a7994b840a74a8d544057267618ed55d2b76b9f6
W-B-S <lws99927800@163.com>
2018-07-03 10:27:51 +0800

    refactoring configuration's identity initialization logic

--- core/core.go
+++ core/core.go
@@ -2,17 +2,17 @@ package core
 
 import (
 	"context"
-	p2p "github.com/ipfs/go-ipfs/p2p"
-	p2phost "gx/ipfs/QmQQGtcp6nVUrQjNsnU53YWV1q8fK1Kd9S7FEkYbRZzxry/go-libp2p-host"
-	goprocess "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
-	discovery "gx/ipfs/QmUEAR2pS7fP1GPseS3i8MWFyENs7oDp4CZrgn8FCjbsBu/go-libp2p/p2p/discovery"
-	psrouter "gx/ipfs/QmVJvKzFi93cWDknZr1UUKyLTJWVRRSixqCqwQ9nLfF8ob/go-libp2p-pubsub-router"
-	peer "gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
+	"github.com/ipfs/go-ipfs/p2p"
+	p2pHost "gx/ipfs/QmQQGtcp6nVUrQjNsnU53YWV1q8fK1Kd9S7FEkYbRZzxry/go-libp2p-host"
+	"gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
+	"gx/ipfs/QmUEAR2pS7fP1GPseS3i8MWFyENs7oDp4CZrgn8FCjbsBu/go-libp2p/p2p/discovery"
+	pubRouter "gx/ipfs/QmVJvKzFi93cWDknZr1UUKyLTJWVRRSixqCqwQ9nLfF8ob/go-libp2p-pubsub-router"
+	"gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
 	ic "gx/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5/go-libp2p-crypto"
-	metrics "gx/ipfs/QmRg1gKTHzc3CZXSKzem8aR4E3TubFhbgXwfVuWnSK5CC5/go-metrics-interface"
-	floodsub "gx/ipfs/QmRFEBGcNjtWPupwHA7zGHeGVLuUyE4ZRFi2MgtrPM6pfb/go-libp2p-floodsub"
-	pstore "gx/ipfs/QmZhsmorLpD9kmQ4ynbAu4vbKv2goMUnXazwGA4gnWHDjB/go-libp2p-peerstore"
-	libp2p "gx/ipfs/QmUEAR2pS7fP1GPseS3i8MWFyENs7oDp4CZrgn8FCjbsBu/go-libp2p"
+	"gx/ipfs/QmRg1gKTHzc3CZXSKzem8aR4E3TubFhbgXwfVuWnSK5CC5/go-metrics-interface"
+	"gx/ipfs/QmRFEBGcNjtWPupwHA7zGHeGVLuUyE4ZRFi2MgtrPM6pfb/go-libp2p-floodsub"
+	peerStore "gx/ipfs/QmZhsmorLpD9kmQ4ynbAu4vbKv2goMUnXazwGA4gnWHDjB/go-libp2p-peerstore"
+	"gx/ipfs/QmUEAR2pS7fP1GPseS3i8MWFyENs7oDp4CZrgn8FCjbsBu/go-libp2p"
 	"github.com/whyrusleeping/go-logging"
 	"fmt"
 )
@@ -20,19 +20,19 @@ import (
 var log = logging.MustGetLogger("nbs/core")
 
 type NbsLightNode struct {
+	ctx  		context.Context
 	identity 	peer.ID
 
 	privateKey  ic.PrivKey
 	discovery  	discovery.Service
 
-	peerStore  	pstore.Peerstore
-	peerHost    p2phost.Host
+	peerStore  	peerStore.Peerstore
+	peerHost    p2pHost.Host
 	floodSub 	*floodsub.PubSub
-	PSRouter 	*psrouter.PubsubValueStore
+	PSRouter 	*pubRouter.PubsubValueStore
 	P2P      	*p2p.P2P
 
-	proc 		goprocess.Process
-	ctx  		context.Context
+	process		goprocess.Process
 }
 
 func NewLightNode(ctx context.Context) (*NbsLightNode, error) {
@@ -56,8 +56,6 @@ func (node *NbsLightNode) Run()  {
 	log.Info("---Start running---")
 
 
-
-
 	defer func() {
 		select {
 			case <-node.ctx.Done():
@@ -68,19 +66,18 @@ func (node *NbsLightNode) Run()  {
 
 func setUpNode(ctx context.Context, node *NbsLightNode) error{
 
-	id, err := GetSysConfig().LoadId()
+	peerId, err := GetSysConfig().LoadId()
 	if err != nil{
 		log.Error("Failed to load node's identity", err)
 		return err
 	}
 
-	node.identity = id
+	node.identity = peerId
+	node.peerStore = peerStore.NewPeerstore()
 
-	node.peerStore = pstore.NewPeerstore()
+	var options []libp2p.Option
 
-	var libp2pOpts []libp2p.Option
-
-	peerHost, err := constructPeerHost(ctx, node.identity, node.peerStore, libp2pOpts...)
+	peerHost, err := constructPeerHost(ctx, node.identity, node.peerStore, options...)
 
 	if err != nil {
 		return err
@@ -96,12 +93,11 @@ func setUpNode(ctx context.Context, node *NbsLightNode) error{
 	return nil
 }
 
-
-func constructPeerHost(ctx context.Context, id peer.ID, ps pstore.Peerstore, options ...libp2p.Option) (p2phost.Host, error) {
-	pkey := ps.PrivKey(id)
-	if pkey == nil {
+func constructPeerHost(ctx context.Context, id peer.ID, ps peerStore.Peerstore, options ...libp2p.Option) (p2pHost.Host, error) {
+	privateKey := ps.PrivKey(id)
+	if privateKey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", id.Pretty())
 	}
-	options = append([]libp2p.Option{libp2p.Identity(pkey), libp2p.Peerstore(ps)}, options...)
+	options = append([]libp2p.Option{libp2p.Identity(privateKey), libp2p.Peerstore(ps)}, options...)
 	return libp2p.New(ctx, options...)
 }
