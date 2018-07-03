1545d71c16df71f24c0909680636f08271d70667
W-B-S <lws99927800@163.com>
2018-07-03 10:27:40 +0800

    refactoring configuration's identity initialization logic

--- core/config.go
+++ core/config.go
@@ -8,7 +8,10 @@ import (
 	"io"
 	"fmt"
 	"github.com/W-B-S/nbs-light-node/errors"
-	peer "gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
+	"gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
+	"crypto/rand"
+	"encoding/base64"
+	ci "gx/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5/go-libp2p-crypto"
 )
 
 const (	LogFileName 	= "NBS-Light-Node.log"
@@ -16,82 +19,146 @@ const (	LogFileName 	= "NBS-Light-Node.log"
 
 var NbsLog = logging.MustGetLogger("core/config")
 
-var SystemConfig *NodeConfig
+var SystemConfig NodeConfig
 
 
 type NodeConfig struct {
-	peerID	string `json:"peerid, omitempty"`
-	privKey string `json:",omitempty"`
-}
-
-var defaultConfig = NodeConfig{
-	peerID:"",
+	PeerID	string `json:"peerId, omitempty"`
+	PrivateKey string `json:",omitempty"`
+	EncryptedPrivateKey string `json:"privateKey,omitempty"`
 }
 
 func init()  {
-	SystemConfig = LoadFromDisk()
+	SystemConfig = initSystemConfig()
 }
 
 func GetSysConfig() (NodeConfig) {
-	return *SystemConfig
+	return SystemConfig
 }
 
 func (config NodeConfig) LoadId() (peer.ID, error){
 
-	cid := config.peerID
-	if cid == "" {
+	peerId := config.PeerID
+	if peerId == "" {
 		return "", errors.New("identity was not set in config")
 	}
 
-	if len(cid) == 0 {
+	if len(peerId) == 0 {
 		return "", errors.New("no peer ID in config! (was 'ipfs init' run?)")
 	}
 
-	id, err := peer.IDB58Decode(cid)
+	decodedId, err := peer.IDB58Decode(peerId)
 
 	if err != nil {
 		return "", fmt.Errorf("peer ID invalid: %s", err)
 	}
 
-	return id, nil
+	return decodedId, nil
 }
 
 func getDefaultPath() string{
 	return utils.GetHomeDir() + ConfigFileName
 }
 
-//TODO::
-func initSystemConfig(){
 
-	defaultConfig.peerID = "100132"
+func (config *NodeConfig)createDefaultIdentity() error{
+
+	privateKey, publicKey, err := ci.GenerateKeyPairWithReader(ci.RSA, 1024, rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	pid, err := peer.IDFromPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+
+	privateKeyBytes, err := privateKey.Bytes()
+	if err != nil {
+		return err
+	}
+
+	config.PeerID = pid.Pretty()
+	config.PrivateKey = base64.StdEncoding.EncodeToString(privateKeyBytes)
+
+//TODO::	save the private key by encrypted data
+//  config.EncryptedPrivateKey = ?
+
+	return nil
 }
 
-func createDefaultConfig(path string)  {
+func (config *NodeConfig) syncToRepo(path string) error{
 
 	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		NbsLog.Error("Failed to create config file:%s", err)
-		return
+		return err
 	}
 
 	defer  logFile.Close()
 
-	initSystemConfig()
-
-	jsonByte, err := json.Marshal(defaultConfig)
+	jsonByte, err := json.Marshal(config)
 	if err != nil{
 		NbsLog.Error("Failed to marshal default config to disk:%s", err)
-		return
+		return err
 	}
 
 	_, err = logFile.Write(jsonByte)
 	if err != nil{
 		NbsLog.Error("Failed to write json content to disk:%s", err)
-		return
+		return err
 	}
+
+	return nil
 }
 
-func LoadFromDisk() (*NodeConfig){
+func createDefaultConfig(path string) (NodeConfig, error) {
+
+	config := NodeConfig{}
+
+	config.createDefaultIdentity()
+
+	err := config.syncToRepo(path)
+	if err != nil{
+		fmt.Errorf("failed to save config to disk!:%s", err.Error())
+		return config, err
+	}
+
+	return config, nil
+}
+
+
+func loadFromRepo(path string) (NodeConfig){
+
+	config := NodeConfig{}
+
+	logFile, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	if err != nil{
+		panic(err)
+	}
+	defer logFile.Close()
+
+	jsonContent := ""
+	buffer := make([]byte, 1024)
+
+	for {
+		number, err := logFile.Read(buffer)
+		if err != nil && err != io.EOF{
+			panic(err)
+		}
+		if 0 == number{
+			break
+		}
+
+		jsonContent = jsonContent + string(buffer[:number])
+	}
+
+	json.Unmarshal([]byte(jsonContent), config)
+
+	return config
+}
+
+func initSystemConfig() (NodeConfig){
 
 	path := getDefaultPath()
 
@@ -100,40 +167,17 @@ func LoadFromDisk() (*NodeConfig){
 	if err != nil {
 		if os.IsNotExist(err) {
 
-			createDefaultConfig(path)
-
-			return &defaultConfig
+			config, err := createDefaultConfig(path)
+			if err != nil{
+				panic(err)
+			}
+			return config
 
 		} else {
 			panic(err)
 		}
-
 	}else{
 
-		logFile, err := os.OpenFile(path, os.O_RDONLY, 0600)
-		if err != nil{
-			panic(err)
-		}
-		defer logFile.Close()
-
-		jsonContent := ""
-		buffer := make([]byte, 1024)
-
-		for {
-			number, err := logFile.Read(buffer)
-			if err != nil && err != io.EOF{
-				panic(err)
-			}
-			if 0 == number{
-				break
-			}
-
-			jsonContent = jsonContent + string(buffer[:number])
-		}
-
-		config := &NodeConfig{}
-		json.Unmarshal([]byte(jsonContent), config)
-
-		return config
+		return loadFromRepo(path)
 	}
 }
